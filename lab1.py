5c965f6411565eb62933690fce393eb38585a8c7
piotrsiedlak <127652415+piotrsiedlak@users.noreply.github.com>
2024-03-02 17:23:05 +0100

    Create lab1.py
    point 1 and 2

---
+++ lab1.py
@@ -0,0 +1,26 @@
+# autor Piotr Siedlak
+# numer albumu 12460
+import datetime
+name = "Piotr" #input("Podaj imię: ")
+age = 30#int(input("Podaj ile masz lat: "))
+print("Cześć "+ name +"!")
+print("Twoje imię ma " + str((len(name))) + " liter i zaczyna się od " + name[0])
+print("Teraz masz " + str(age) + "lat, a za rok będzie to " + str(age+1) + ". Rok urodzenia to : " + str(datetime.datetime.now().year-age))
+
+age = 12#int(input("Podaj ile masz lat: "))
+money = 10#int(input("Podaj ile masz siana: "))
+if (age >= 18) and (money >= 20):
+    print("Jesteś pełnoletni i masz wystarczającą ilość pieniędz")
+elif (age >= 18) and (money < 20):
+    print("Brakuje Ci " + str(20-money) + " zł")
+elif (age < 18) and (money >= 20):
+    print("Brakuje Ci " + str(18-age) + " lat do pełnoletności.")
+else:
+    print("Brakuje Ci " + str(20-money) + " zł oraz "+ str(18-age) + " lat do pełnoletności.")
+
+# Napisz prosty kalkulator podatkowy dla podatku dochodowego. Najpierw program niech zapyta użytkownika, czy chce podać dochód miesięczny czy roczny. W przypadku wybrania 
+# dochodu miesięcznego, oblicz dochód roczny. Następnie na podstawie podanych wartości oblicz wartość rocznego podatku dochodowego w oparciu o I i II próg podatkowy 
+# (12% i 32%) - sposób obliczania podatku według progów podatkowych znajdziesz w Internecie. Pomiń kwestie kwoty wolnej od podatku oraz założeń Polskiego Ładu.
+
+is_yearly = input()
+income = input("Podaj ")
