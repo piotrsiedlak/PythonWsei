3565ed977fe2225c064ee35a14ea272cab6bfd0c
piotrsiedlak <piotr.siedlak@microsoft.wsei.edu.pl>
2024-03-02 19:08:50 +0100

    almost all 5

--- lab1.py
+++ lab1.py
@@ -1,8 +1,9 @@
 # autor Piotr Siedlak
 # numer albumu 12460
 
-""" import datetime
-name = "Piotr" #input("Podaj imię: ")
+import datetime
+import calendar
+""" name = "Piotr" #input("Podaj imię: ")
 age = 30#int(input("Podaj ile masz lat: "))
 print("Cześć "+ name +"!")
 print("Twoje imię ma " + str((len(name))) + " liter i zaczyna się od " + name[0])
@@ -20,7 +21,7 @@ else:
     print("Brakuje Ci " + str(20-money) + " zł oraz "+ str(18-age) + " lat do pełnoletności.")
  """
 
-is_yearly = input("Podaj sposób podawania dochodu roczny (R) albo miesięczny (M): ")
+""" is_yearly = input("Podaj sposób podawania dochodu roczny (R) albo miesięczny (M): ")
 if is_yearly.upper() == "R":
     is_yearly = True
 else:
@@ -33,7 +34,34 @@ if not is_yearly:
 
 tax_bracket = 120000
 if(income <= 120000):
-    tax_to_pay = 0.12*income
+    print(f"Jesteś w pierwszym progu podatkowym. Twój podatek do zapłacenia to: {0.12*income} zł")
 else:
-    tax_to_pay = 0.32*income
+    tax_to_pay = 0.12*120000 + 0.32*(income - 120000)
+    print(f"Jesteś w drugim progu podatkowym. Twój podatek do zapłacenia to: {tax_to_pay} zł") """
+    
+
+# Napisz prosty program do obliczania pola figur geometrycznych. Użytkownik proszony jest o podanie nazwy figury w formie stringa (np. “prostokąt”, “trójkąt”, “koło” itd. - wersja 
+# polska lub angielska), następnie w zależności od wybranej figury musi podać parametry - bok, średnica, wysokość itp. Przed obliczeniami wartości muszą być sprawdzone (np. > 0). 
+# Po wpisaniu wszystkich wartości program wyświetla pole figury w formie pełnej odpowiedzi (np. “Pole figury <wybrana_figura> wynosi: <pole>”). Zadbaj o odpowiednie nazwy 
+# zmiennych (po angielsku!) oraz rzutowanie typów. 
+# UWAGA: Program wydaje się “szkolny”, ale będziemy do niego wielokrotnie wracać - przy pętlach dodamy do niego ponowne proszenie o podanie liczby, potem przeniesiemy
+# obliczenia do funkcji, dodamy obsługę wyjątków, napiszemy to z wykorzystaniem OOP, a chętni mogą nawet podpiąć Turtle i narysować te figury. Ale cierpliwości :-)
+    
+
+# Zdefiniuj słownik, którego wartościami będą wydatki na życie w ostatnich kilku miesiącach, a kluczami nazwy miesięcy. Wyznacz i wyświetl wartość minimalną, maksymalną, sumę i 
+# wartość średnią (wykonaj odpowiednie operacje na liście wartości). Sprawdź czy kwota za ostatni miesiąc przekracza wartość średnią - jeśli tak, to wyświetl tekst ostrzeżenia 
+# “zacznij oszczędzać”, a jeśli nie, informację “jesteś bezpieczny”. Używając pętli wyświetl elementy słownika, każdy w innej linii, ale tylko jeśli przekraczają wartość średnią.
+    
+expensess = {"January" : 1200, "February" : 1500, "March" : 1000, "April" : 1900, "May" : 1800, "June" : 2000, "July" : 1600, "August" : 1500, "September" : 1700, "October" : 1800, "November" : 1500, "December" : 2200}
+print(f"Wartość minimalna wydatków to: {min(expensess.values())} zł")
+print(f"Wartość maksymalna wydatków to: {max(expensess.values())} zł")
+print(f"Suma wydatków to: {sum(expensess.values())} zł")
+average_expensess = sum(expensess.values())/12
+print(f"Średnia wydatków to: {average_expensess:.2f} zł")
+
+if expensess[calendar.month_name[datetime.datetime.now().month]] > average_expensess:
+    print("Zacznij oszczędzać!")
+else:
+    print("Jesteś bezpieczny")
+
 
