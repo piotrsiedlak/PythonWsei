59e1ef393eb3a84247d7b5e9e9c97bd619935211
piotrsiedlak <piotr.siedlak@microsoft.wsei.edu.pl>
2024-03-02 17:35:01 +0100

    two types of income input

--- lab1.py
+++ lab1.py
@@ -1,6 +1,7 @@
 # autor Piotr Siedlak
 # numer albumu 12460
-import datetime
+
+""" import datetime
 name = "Piotr" #input("Podaj imię: ")
 age = 30#int(input("Podaj ile masz lat: "))
 print("Cześć "+ name +"!")
@@ -17,10 +18,20 @@ elif (age < 18) and (money >= 20):
     print("Brakuje Ci " + str(18-age) + " lat do pełnoletności.")
 else:
     print("Brakuje Ci " + str(20-money) + " zł oraz "+ str(18-age) + " lat do pełnoletności.")
-
+ """
 # Napisz prosty kalkulator podatkowy dla podatku dochodowego. Najpierw program niech zapyta użytkownika, czy chce podać dochód miesięczny czy roczny. W przypadku wybrania 
 # dochodu miesięcznego, oblicz dochód roczny. Następnie na podstawie podanych wartości oblicz wartość rocznego podatku dochodowego w oparciu o I i II próg podatkowy 
 # (12% i 32%) - sposób obliczania podatku według progów podatkowych znajdziesz w Internecie. Pomiń kwestie kwoty wolnej od podatku oraz założeń Polskiego Ładu.
 
-is_yearly = input()
-income = input("Podaj ")
+is_yearly = input("Podaj sposób podawania dochodu roczny (R) albo miesięczny (M): ")
+if is_yearly.upper() == "R":
+    is_yearly = True
+else:
+    is_yearly = False
+
+income = int(input("Podaj dochód brutto: "))
+
+if not is_yearly:
+    income *= 12
+
+
